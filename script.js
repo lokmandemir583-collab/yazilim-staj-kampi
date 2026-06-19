b09e321dc5389bfcb132794915beff1dbf3af13b
Lokman Demir <lokmandemir583@gmail.com>
2026-06-19 14:14:03 +0300

    javascript sayac eklendi

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const buton = document.getElementById("baslaBtn");
 const mesaj = document.getElementById("mesaj");
+const sayac = document.getElementById("sayac");
+
+let tiklamaSayisi = 0;
 
 buton.addEventListener("click", function() {
+  tiklamaSayisi = tiklamaSayisi + 1;
+
   mesaj.textContent = "Harika! JavaScript çalışıyor. 3. güne başladım.";
+  sayac.textContent = "Butona " + tiklamaSayisi + " kez tıkladın.";
 });

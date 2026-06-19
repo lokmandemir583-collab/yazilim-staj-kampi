057006dc600565c00c51f93727dfb770000e29e1
Lokman Demir <lokmandemir583@gmail.com>
2026-06-19 14:21:40 +0300

    javascript input ve kosul eklendi

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ const buton = document.getElementById("baslaBtn");
 const mesaj = document.getElementById("mesaj");
 const sayac = document.getElementById("sayac");
 
+const isimInput = document.getElementById("isimInput");
+const isimBtn = document.getElementById("isimBtn");
+const karsilama = document.getElementById("karsilama");
+
 let tiklamaSayisi = 0;
 
 buton.addEventListener("click", function() {
@@ -9,4 +13,14 @@ buton.addEventListener("click", function() {
 
   mesaj.textContent = "Harika! JavaScript çalışıyor. 3. güne başladım.";
   sayac.textContent = "Butona " + tiklamaSayisi + " kez tıkladın.";
+});
+
+isimBtn.addEventListener("click", function() {
+  const isim = isimInput.value;
+
+  if (isim === "") {
+    karsilama.textContent = "Lütfen önce adını yaz.";
+  } else {
+    karsilama.textContent = "Merhaba " + isim + ", yazılım staj kampına hoş geldin!";
+  }
 });

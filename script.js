a7055fa9eac5dc6339909099fce7de558a219e7b
Lokman Demir <lokmandemir583@gmail.com>
2026-06-19 14:09:22 +0300

    javascript buton etkilesimi eklendi

---
+++ script.js
@@ -0,0 +1,6 @@
+const buton = document.getElementById("baslaBtn");
+const mesaj = document.getElementById("mesaj");
+
+buton.addEventListener("click", function() {
+  mesaj.textContent = "Harika! JavaScript çalışıyor. 3. güne başladım.";
+});
